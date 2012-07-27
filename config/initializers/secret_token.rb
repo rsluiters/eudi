609e80ffc7292cf43f89d50bbbf07853ccdd07d5
Sluiters <rsluiters@ratws217mbp.fritz.box>
2012-07-27 19:14:33 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eudi::Application.config.secret_token = 'a54c06a38751ef349d77abf54491cbfe56aa1db3a5882240e662ed55946b6abaa91a635097a24bea0bffed838030ed2305fd650c13d06ec3868ce58c1167f7da'
