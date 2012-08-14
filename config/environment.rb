733e7ef5b381e775f11b3a90ea157d2fc707475d
Ralph Sluiters <ralph.sluiters@invision.de>
2012-08-14 23:02:34 +0200

    Removed PDF for now part 2

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Eudi::Application.initialize!
 
-require 'pdf/writer'
-Mime::Type.register 'application/pdf', :pdf
+#require 'pdf/writer'
+#Mime::Type.register 'application/pdf', :pdf
